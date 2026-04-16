963570c445b24515762f1aad856e46d12f5eac78
breynard <dev@breynard.net>
2026-04-16 20:27:55 -0300

    Removed sound popup

--- home.nix
+++ home.nix
@@ -19,82 +19,77 @@
   home.username = "breynard";
 
   # Packages that should be installed to the user profile.
-  home.packages =
-    with pkgs;
-    [
-      fastfetch
+  home.packages = with pkgs; [
+    fastfetch
 
-      # archives
-      zip
-      xz
-      unzip
-      p7zip
+    # archives
+    zip
+    xz
+    unzip
+    p7zip
 
-      # utils
-      fish
-      starship
-      ripgrep # recursively searches directories for a regex pattern
-      jq # A lightweight and flexible command-line JSON processor
-      yq-go # yaml processor https://github.com/mikefarah/yq
-      eza # A modern replacement for ‘ls’
-      fzf # A command-line fuzzy finder
+    # utils
+    fish
+    starship
+    ripgrep # recursively searches directories for a regex pattern
+    jq # A lightweight and flexible command-line JSON processor
+    yq-go # yaml processor https://github.com/mikefarah/yq
+    eza # A modern replacement for ‘ls’
+    fzf # A command-line fuzzy finder
 
-      # networking tools
-      mtr # A network diagnostic tool
-      iperf3
-      dnsutils # `dig` + `nslookup`
-      ldns # replacement of `dig`, it provide the command `drill`
-      aria2 # A lightweight multi-protocol & multi-source command-line download utility
-      socat # replacement of openbsd-netcat
-      nmap # A utility for network discovery and security auditing
-      ipcalc # it is a calculator for the IPv4/v6 addresses
+    # networking tools
+    mtr # A network diagnostic tool
+    iperf3
+    dnsutils # `dig` + `nslookup`
+    ldns # replacement of `dig`, it provide the command `drill`
+    aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    socat # replacement of openbsd-netcat
+    nmap # A utility for network discovery and security auditing
+    ipcalc # it is a calculator for the IPv4/v6 addresses
 
-      # misc
-      cowsay
-      file
-      which
-      tree
-      gnused
-      gnutar
-      gawk
-      zstd
-      gnupg
-      dconf
+    # misc
+    cowsay
+    file
+    which
+    tree
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
+    dconf
 
-      # nix related
-      #
-      # it provides the command `nom` works just like `nix`
-      # with more details log output
-      nix-output-monitor
+    # nix related
+    #
+    # it provides the command `nom` works just like `nix`
+    # with more details log output
+    nix-output-monitor
 
-      # productivity
-      htop
-      iotop # io monitoring
-      iftop # network monitoring
+    # productivity
+    htop
+    iotop # io monitoring
+    iftop # network monitoring
 
-      # system call monitoring
-      strace # system call monitoring
-      ltrace # library call monitoring
-      lsof # list open files
+    # system call monitoring
+    strace # system call monitoring
+    ltrace # library call monitoring
+    lsof # list open files
 
-      # system tools
-      sysstat
-      lm_sensors # for `sensors` command
-      ethtool
-      pciutils # lspci
-      usbutils # lsusb
+    # system tools
+    sysstat
+    lm_sensors # for `sensors` command
+    ethtool
+    pciutils # lspci
+    usbutils # lsusb
 
-      # nix dev tools
-      nixd
-      nixfmt
-      nh
+    # nix dev tools
+    nixd
+    nixfmt
+    nh
 
-      # Fonts
-      nerd-fonts.jetbrains-mono
-    ]
-    ++ [
-      (pkgs.callPackage ./pkgs/sound_popup.nix { })
-    ];
+    # Fonts
+    nerd-fonts.jetbrains-mono
+  ];
 
   # basic configuration of git, please change to your own
   programs.git = {
