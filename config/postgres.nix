647ad1b7aa6245f134b3acb64f9bf87659aaed63
breynard <dev@breynard.net>
2026-08-28 12:22:02 -0400

    Enable TCP/IP auth for postgres

--- config/postgres.nix
+++ config/postgres.nix
@@ -3,9 +3,13 @@
   config.services.postgresql = {
     enable = true;
     ensureDatabases = [ "devbase" ];
+    enableTCPIP = true;
+    settings.password_encryption = "scram-sha-256";
     authentication = pkgs.lib.mkOverride 10 ''
-      #type database  DBuser  auth-method
-      local all       all     trust
+      #type database  DBuser  address        auth-method
+      local all       all                    trust
+      host  all       all     127.0.0.1/32   scram-sha-256
+      host  all       all     ::1/128        scram-sha-256
     '';
     package = pkgs.postgresql_16;
   };
